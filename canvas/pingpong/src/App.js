d8032ceba73ca52086e1accb748114aad90d2de7
Mongkonchai Priyachiwa <mongkonchai4412@gmail.com>
2019-04-15 19:42:30 +0700

    add brick collision

--- canvas/pingpong/src/App.js
+++ canvas/pingpong/src/App.js
@@ -4,11 +4,19 @@ import './App.css'
 function GameCanvas() {
   const canvasRef = useRef(null)
   let canvas, canvasContext
+  let mouseX, mouseY
 
   let ballX = 75
   let ballY = 75
   let ballSpeedX = 5
   let ballSpeedY = 7
+
+  const BRICK_W = 80
+  const BRICK_H = 20
+  const BRICK_GAP = 2
+  const BRICK_COLS = 10
+  const BRICK_ROWS = 14
+  let brickGrid = new Array(BRICK_COLS * BRICK_ROWS)
 
   const PADDLE_WIDTH = 100
   const PADDLE_THICKNESS = 10
@@ -22,6 +30,7 @@ function GameCanvas() {
     setInterval(updateAll, 1000 / framesPerSecond)
 
     canvas.addEventListener('mousemove', updateMousePos)
+    brickReset()
   }, [])
 
   const updateMousePos = (e) => {
@@ -38,12 +47,18 @@ function GameCanvas() {
     drawAll()
   }
 
+  const brickReset = () => {
+    for (let i = 0; i < BRICK_COLS * BRICK_ROWS; i++) {
+      brickGrid[i] = true
+    }
+  }
+
   const ballReset = () => {
     ballX = canvas.width / 2
     ballY = canvas.height / 2
   }
 
-  const moveAll = () => {
+  const ballMove = () => {
     ballX += ballSpeedX
     ballY += ballSpeedY
 
@@ -56,7 +71,25 @@ function GameCanvas() {
     if (ballY > canvas.height) {
       ballReset()
     }
+  }
 
+  const ballBrickHandling = () => {
+    const ballBrickCol = Math.floor(ballX / BRICK_W)
+    const ballBrickRow = Math.floor(ballY / BRICK_H)
+    const brickIndexUnderBall = rowColToArrayIndex(ballBrickCol, ballBrickRow)
+    colorText(`${ballBrickCol},${ballBrickRow}:${brickIndexUnderBall}`, mouseX, mouseY, 'yellow')
+
+    if (
+      ballBrickCol >= 0 && ballBrickCol < BRICK_COLS &&
+      ballBrickRow >= 0 && ballBrickRow < BRICK_ROWS) {
+      if (brickGrid[brickIndexUnderBall]) {
+        brickGrid[brickIndexUnderBall] = false
+        ballSpeedY *= -1
+      }
+    }
+  }
+
+  const ballPaddleHandling = () => {
     let paddleTopEdgeY = canvas.height - PADDLE_DIST_FROM_EDGE
     let paddleBottomEdgeY = paddleTopEdgeY + PADDLE_THICKNESS
     let paddleLeftEdgeX = paddleX
@@ -74,10 +107,32 @@ function GameCanvas() {
     }
   }
 
+  const moveAll = () => {
+    ballMove()
+    ballBrickHandling()
+    ballPaddleHandling()
+  }
+
+  const rowColToArrayIndex = (col, row) => {
+    return col + BRICK_COLS * row
+  }
+
+  const drawBricks = () => {
+    for (let eachRow = 0; eachRow < BRICK_ROWS; eachRow++) {
+      for (let eachCol = 0; eachCol < BRICK_COLS; eachCol++) {
+        const arrayIndex = rowColToArrayIndex(eachCol, eachRow)
+        if (brickGrid[arrayIndex]) {
+          colorRect(BRICK_W * eachCol, BRICK_H * eachRow, BRICK_W - BRICK_GAP, BRICK_H - BRICK_GAP, 'blue')
+        }
+      }
+    }
+  }
+
   const drawAll = () => {
     colorRect(0, 0, canvas.width, canvas.height, 'black')
     colorCircle(ballX, ballY, 10, 'white')
     colorRect(paddleX, canvas.height - PADDLE_DIST_FROM_EDGE, PADDLE_WIDTH, PADDLE_THICKNESS)
+    drawBricks()
   }
 
   const colorRect = (topLeftX, topLeftY, boxWidth, boxHeight, fillColor) => {
@@ -90,6 +145,11 @@ function GameCanvas() {
     canvasContext.beginPath()
     canvasContext.arc(centerX, centerY, radius, 0, 2 * Math.PI)
     canvasContext.fill()
+  }
+
+  const colorText = (showWords, textX, textY, fillColor) => {
+    canvasContext.fillStyle = fillColor
+    canvasContext.fillText(showWords, textX, textY)
   }
 
   return (
