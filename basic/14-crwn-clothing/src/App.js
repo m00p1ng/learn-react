7bcf2cebd3ae6092c73b76742f8df1ce6c8f864e
Mongkonchai Priyachiwa <mongkonchai4412@gmail.com>
2019-07-11 23:27:04 +0700

    add simple auth

--- basic/14-crwn-clothing/src/App.js
+++ basic/14-crwn-clothing/src/App.js
@@ -5,20 +5,41 @@ import HomePage from './pages/homepage/Homepage';
 import ShopPage from './pages/shop/Shop';
 import Header from './components/header/Header';
 import SignInAndSignUp from './pages/sign-in-and-sign-up/SignInAndSignUp';
+import { auth } from './firebase/firebase.utils';
 
 import './App.css';
 
-function App() {
-  return (
-    <React.Fragment>
-      <Header />
-      <Switch>
-        <Route exact path="/" component={HomePage} />
-        <Route exact path="/shop" component={ShopPage} />
-        <Route exact path="/signin" component={SignInAndSignUp} />
-      </Switch>
-    </React.Fragment>
-  );
+class App extends React.Component {
+  state = {
+    currentUser: null,
+  };
+
+  unsubscribeFromAuth = null;
+
+  componentDidMount() {
+    this.unsubscribeFromAuth = auth.onAuthStateChanged((user) => {
+      this.setState({ currentUser: user });
+
+      console.log({ user });
+    });
+  }
+
+  componentWillUnmount() {
+    this.unsubscribeFromAuth();
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+        <Header currentUser={this.state.currentUser} />
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route path="/shop" component={ShopPage} />
+          <Route path="/signin" component={SignInAndSignUp} />
+        </Switch>
+      </React.Fragment>
+    );
+  }
 }
 
 export default App;
--- basic/14-crwn-clothing/src/components/sign-in/SignIn.js
+++ basic/14-crwn-clothing/src/components/sign-in/SignIn.js
@@ -3,6 +3,8 @@ import React from 'react';
 import FormInput from '../form-input/FormInput';
 import CustomButton from '../custom-button/CustomButton';
 
+import { signInWithGoogle } from '../../firebase/firebase.utils';
+
 import './SignIn.scss';
 
 class SignIn extends React.Component {
@@ -29,22 +31,29 @@ class SignIn extends React.Component {
         <h2>I already have an account</h2>
         <span>Sign in with your email and password</span>
 
-        <form action="">
+        <form onSubmit={this.handleSubmit}>
           <FormInput
-            type="email"
             name="email"
+            type="email"
+            handleChange={this.handleChange}
             value={this.state.email}
             label="email"
-            onChange={this.handleChange}
+            required
           />
           <FormInput
-            type="password"
             name="password"
+            type="password"
             value={this.state.password}
+            handleChange={this.handleChange}
             label="password"
-            onChange={this.handleChange}
+            required
           />
-          <CustomButton type="submit">Sign In</CustomButton>
+          <div className="buttons">
+            <CustomButton type="submit"> Sign in </CustomButton>
+            <CustomButton onClick={signInWithGoogle} isGoogleSignIn>
+              Sign in with Google
+            </CustomButton>
+          </div>
         </form>
       </div>
     );
