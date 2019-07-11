e8dab53a6c6f0ac7f3e80096a8946e739595ba4c
Mongkonchai Priyachiwa <mongkonchai4412@gmail.com>
2019-07-11 21:32:58 +0700

    add simple route

--- basic/14-crwn-clothing/src/App.js
+++ basic/14-crwn-clothing/src/App.js
@@ -1,10 +1,22 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
 import HomePage from './pages/homepage/Homepage';
+import ShopPage from './pages/shop/Shop';
+import Header from './components/header/Header';
 
 import './App.css';
 
 function App() {
-  return <HomePage />;
+  return (
+    <React.Fragment>
+      <Header />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route exact path="/shop" component={ShopPage} />
+      </Switch>
+    </React.Fragment>
+  );
 }
 
 export default App;
