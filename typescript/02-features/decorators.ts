ce6401a3c26b7399880f46e6dc1c508866c56e2b
Mongkonchai Priyachiwa <mongkonchai4412@gmail.com>
2019-07-16 21:58:50 +0700

    add simple decorator

---
+++ typescript/02-features/decorators.ts
@@ -0,0 +1,43 @@
+class Boat {
+  @testDecorator
+  color: string = 'red';
+
+  get formattedColor(): string {
+    return `This boats color is ${this.color}`;
+  }
+
+  @logError('Oops boat was sunk in ocean')
+  pilot(
+    @parameterDecorator speed: string,
+    @parameterDecorator generateWake: boolean,
+  ): void {
+    if (speed === 'fast') {
+      console.log('swish');
+    } else {
+      console.log('nothing');
+    }
+  }
+}
+
+function parameterDecorator(target: any, key: string, index: number) {
+  console.log(key, index);
+}
+
+function testDecorator(target: any, key: string) {
+  console.log(target);
+  console.log(key);
+}
+
+function logError(errorMessage: string) {
+  return function(target: any, key: string, desc: PropertyDescriptor): void {
+    const method = desc.value;
+
+    desc.value = function() {
+      try {
+        method();
+      } catch (e) {
+        console.log(errorMessage);
+      }
+    };
+  };
+}
