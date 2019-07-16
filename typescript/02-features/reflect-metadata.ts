b315e1197b8d49e4716d4216db5874014deee747
Mongkonchai Priyachiwa <mongkonchai4412@gmail.com>
2019-07-16 23:13:09 +0700

    add simple reflect

---
+++ typescript/02-features/reflect-metadata.ts
@@ -0,0 +1,44 @@
+import 'reflect-metadata';
+
+const plane = {
+  color: 'red',
+};
+
+// Reflect.defineMetadata('note', 'hi there', plane);
+
+// const note = Reflect.getMetadata('note', plane);
+
+// console.log(note);
+
+Reflect.defineMetadata('note', 'hi there', plane, 'color');
+
+const note = Reflect.getMetadata('note', plane, 'color');
+
+// Class example
+
+@printMetadata
+class Plane {
+  color: string = 'red';
+
+  @markFunction('HI THERE')
+  fly(): void {
+    console.log('vrrrr');
+  }
+}
+
+function markFunction(secretInfo: string) {
+  return function(target: Plane, key: string) {
+    Reflect.defineMetadata('secret', secretInfo, target, key);
+  };
+}
+
+function printMetadata(target: typeof Plane) {
+  for (let key in target.prototype) {
+    const secret = Reflect.getMetadata('secret', target.prototype, key);
+    console.log(secret);
+  }
+}
+
+const secret = Reflect.getMetadata('secret', Plane.prototype, 'fly');
+
+console.log(secret);
