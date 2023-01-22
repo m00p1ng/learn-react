2bab3a20be2dc5c2495eccae3a17fa7c41d29382
m00p1ng <mongkonchai4412@gmail.com>
2023-01-22 18:24:15 +0700

    update remix note

--- remix/app/root.tsx
+++ remix/app/root.tsx
@@ -1,12 +1,17 @@
 import type { MetaFunction } from "@remix-run/node";
 import {
+    Link,
   Links,
   LiveReload,
   Meta,
   Outlet,
   Scripts,
   ScrollRestoration,
+  useCatch,
 } from "@remix-run/react";
+
+import styles from '~/styles/main.css';
+import MainNavigation from "~/components/MainNavigation";
 
 export const meta: MetaFunction = () => ({
   charset: "utf-8",
@@ -22,6 +27,9 @@ export default function App() {
         <Links />
       </head>
       <body>
+        <header>
+          <MainNavigation />
+        </header>
         <Outlet />
         <ScrollRestoration />
         <Scripts />
@@ -29,4 +37,64 @@ export default function App() {
       </body>
     </html>
   );
+}
+
+export function CatchBoundary() {
+  const caughtResponse = useCatch();
+  return (
+    <html lang="en">
+      <head>
+        <Meta />
+        <Links />
+        <title>{caughtResponse.statusText}</title>
+      </head>
+      <body>
+        <header>
+          <MainNavigation />
+        </header>
+        <main className="error">
+          <h1>{caughtResponse.statusText}</h1>
+          <p>{caughtResponse.data?.message || 'Something went wrong!'}</p>
+          <p>
+            Back to <Link to="/">safety</Link>!
+          </p>
+        </main>
+        <ScrollRestoration />
+        <Scripts />
+        <LiveReload />
+      </body>
+    </html>
+  );
+}
+
+export function ErrorBoundary({ error }: any) {
+  return (
+    <html lang="en">
+      <head>
+        <Meta />
+        <Links />
+        <title>An error occurred!</title>
+      </head>
+      <body>
+        <header>
+          <MainNavigation />
+        </header>
+        <main className="error">
+          <h1>An error occurred!</h1>
+          <p>{error.message}</p>
+          <p>
+            Back to <Link to="/">safety</Link>!
+          </p>
+        </main>
+        <ScrollRestoration />
+        <Scripts />
+        <LiveReload />
+      </body>
+    </html>
+  );
+}
+
+
+export function links() {
+  return [{ rel: 'stylesheet', href: styles }]
 }
