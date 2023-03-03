727d04a3e62abf8c5cdf0755e4dff78c279d24a4
m00p1ng <mongkonchai4412@gmail.com>
2023-03-04 00:20:07 +0700

    init shoppay

---
+++ shoppay/pages/index.tsx
@@ -0,0 +1,14 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Hello</title>
+      </Head>
+      <main>
+        Hello
+      </main>
+    </>
+  )
+}
